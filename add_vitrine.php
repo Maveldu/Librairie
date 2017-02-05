68714f24f2dcb4a809d3d8c82648b1e6889bc261
GabinV <g.ventrinponte@outlook.fr>
2017-02-05 12:22:11 +0100

    ajout style dans add_vitrine

--- add_vitrine.php
+++ add_vitrine.php
@@ -1,9 +1,9 @@
 <?php
-
+session_start();
 require_once 'fonc_bdd.php';
 $bdd = OuvrirConnexion($session, $usr, $mdp);
 $titre = "Librairie"; //Titre ‡ changer sur chaque page
-
+require_once 'menu.php';
 
 $isbn = str_replace('-', '', $_POST['vitrineadd']);
 $num = $bdd->query('SELECT NUM from vitrine');
@@ -11,8 +11,12 @@ $count = $num->rowCount();
 $count = $count + 1;
 //echo $isbn;
 $bdd->exec('INSERT INTO vitrine(ISBN_ISSN, NUM) VALUES(\'' . $isbn . '\'  ,    \'' . $count . '\'             );');
-echo "Votre Photo a bien été ajouté à la vitrine."
+
 ?>
 <html>
+<link rel="icon" type="image/png" href="favicon.png"/>
+<br><br><br><br><br><center>
+    <p>Le Livre a bien été ajouté à la vitrine.</p>
 <input class="btn btn-default" type="button" name="Accueil" value="Accueil" onclick="self.location.href='index.php'">
+</center><
 </html>
