9d525b4f888a00c400c2aef3e17be36dcc2f9f09
GabinV <g.ventrinponte@outlook.fr>
2017-03-23 13:10:37 +0100

    MAJ BDD CLEAN

--- database/librairie4_0.sql
+++ database/librairie4_0.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 23 Mars 2017 à 09:56
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Généré le :  Jeu 23 Mars 2017 à 13:09
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -50,7 +50,7 @@ CREATE TABLE `article` (
 
 INSERT INTO `article` (`ISBN_ISSN`, `ID_EDITEUR`, `TITRE`, `QUANTITE_STOCK`, `NOM_EDITEUR`, `COLLECTION`, `DATE_PARUTION`, `NUMERO_COLLECTION`, `EDITION`, `PRIX`, `RESUME`, `NOTE_GERANT`, `SUPPORT`, `Couverture`, `MOTCLES`) VALUES
 (9782917437520, 917437, 'Ces cris gravés', 50, 'Éditions du Chameau', 'Éditions du Chameau', '2016-01-06', 1, 'Éditions du Chameau', '10.00', 'Dans le château de Thunder-ten-tronckh, Pangloss, le maître de Candide, lui enseigne que tout va pour le mieux dans le meilleur des mondes. Candide le croit, mais se fait chasser du château pour un baiser donné à sa cousine Cunégonde.', '', 'choix2', '', 'photographie, dessin,texte'),
-(9782917437605, 917437, 'Slams', 3, 'Éditions du Chameau', 'Éditions du Chameau', '2016-01-08', 1, 'Éditions du Chameau', '15.00', '1815. Alors que tous les aubergistes de la ville l\'ont chassé, le bagnard Jean Valjean est hébergé par Mgr Myriel ( que les pauvres ont baptisé, d\'après l\'un de ses prénoms, Mgr Bienvenu). L\'évêque de la ville de Digne, l\'accueille avec bienveillance, le', 'Bon livre apprécié par notre équipe', 'papier', '', ''),
+(9782917437605, 917437, 'Slams', 3, 'Éditions du Chameau', 'Éditions du Chameau', '2016-01-08', 1, 'Éditions du Chameau', '15.00', '1815. Alors que tous les aubergistes de la ville l''ont chassé, le bagnard Jean Valjean est hébergé par Mgr Myriel ( que les pauvres ont baptisé, d''après l''un de ses prénoms, Mgr Bienvenu). L''évêque de la ville de Digne, l''accueille avec bienveillance, le', 'Bon livre apprécié par notre équipe', 'papier', '', ''),
 (9782917437742, 917437, 'Pourquoi y a t-il...?', 42, 'Éditions du chameau', 'Éditions du chameau', '2016-12-01', 125, 'Éditions du chameau', '14.00', 'Une femme un peu étrange qui balaye un théâtre, mais pas trop, qui parle, beaucoup, qui s’interroge, se scandalise et s’émerveille de l’absurde logique qui semble régir les hommes...', NULL, 'papier', '', ''),
 (9782917437773, 917437, 'Le goût des hommes', 25, 'Éditions du chameau', 'Éditions du chameau', '2016-12-01', 125, 'Éditions du chameau', '15.00', '« Je ne savais pas que ce goût serait si difficile à vivre et encore plus difficile à assumer » écrit Janine Mesnildrey dès la première page de son roman.', NULL, 'papier', '', '');
 
@@ -71,9 +71,9 @@ CREATE TABLE `auteur` (
 --
 
 INSERT INTO `auteur` (`ID_AUTEUR`, `NOM_AUTEUR`, `PRENOM_AUTEUR`) VALUES
-(1, 'Thouroude', 'Simonne'),
-(2, 'kendji', 'Jirac'),
-(3, 'lol', 'lol');
+(1, 'Hugo', 'Victor'),
+(2, 'Camus', 'Albert'),
+(3, 'Zola', 'Emile');
 
 -- --------------------------------------------------------
 
@@ -92,27 +92,10 @@ CREATE TABLE `client` (
 --
 
 INSERT INTO `client` (`NUMERO_COMPTE`, `NOM`, `PRENOM`) VALUES
-(4, 'lucas', 'lucas'),
-(5, 'gabin', 'ventrinponte'),
-(9, 'BOISEAU', 'ADRIEN'),
-(10, 'savary', 'lucas'),
-(11, 'editeur', 'profe'),
-(12, 'editeur', 'profe'),
-(13, 'pro', 'fesse'),
-(14, 'clent', 'client'),
-(15, 'Livre', 'JeanMichel'),
-(16, 'test', 'tes'),
-(17, 'jeanmichel', 'panier'),
-(18, 'commande', 'commande'),
-(19, 'Client', 'Clientpre'),
-(20, 'Clientpro', 'clientpropre'),
-(21, 'testrei', 'testreifez'),
-(23, 'glout', 'glout'),
-(24, 'glouto', 'glouto'),
-(25, 'glout', 'glout'),
-(26, 'testd', 'Glouton'),
-(27, 'Gabin', 'Glouton'),
-(31, 'PEUSDO', 'Jeanclaude');
+(1, 'Client', 'Client'),
+(2, 'Clientpro', 'Clientpro'),
+(3, 'Fournisseur', 'Fournisseur'),
+(4, 'Fournisseur', 'Fournisseur');
 
 -- --------------------------------------------------------
 
@@ -132,40 +115,21 @@ CREATE TABLE `commande` (
 --
 
 INSERT INTO `commande` (`NUMERO_COMMANDE`, `NUMERO_COMPTE`, `DATE_COMMANDE`, `ETAT_COMMANDE`) VALUES
-(1, 15, NULL, NULL),
-(2, 16, NULL, 'REFUSE'),
-(3, 12, NULL, 'REFUSE'),
-(4, 10, NULL, 'REFUSE'),
-(5, 6, NULL, 'REFUSE'),
-(6, 4, NULL, 'REFUSE'),
-(7, 3, NULL, 'REFUSE'),
-(12, 2, NULL, 'REFUSE'),
-(16, 18, '0000-00-00', 'VALIDE'),
-(17, 8, NULL, 'EN COURS'),
-(199, 0, '2017-01-04', 'EN COURS'),
-(218, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(219, 21, '20-01-2017', 'REFUSE'),
-(221, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(222, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(223, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(224, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(225, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(226, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(227, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(228, 17, '20-01-2017', 'EN ATTENTE DE VALIDATION'),
-(229, 17, '20-01-2017', 'ANNULE'),
-(230, 17, '20-01-2017', 'ANNULE'),
-(231, 17, '20-01-2017', 'ANNULE'),
-(232, 17, '30-01-2017', 'ANNULE'),
-(233, 17, '16-02-2017', 'ANNULE'),
-(234, 22, NULL, 'EN COURS'),
-(235, 23, NULL, 'EN COURS'),
-(236, 24, NULL, 'EN COURS'),
-(237, 25, NULL, 'EN COURS'),
-(238, 26, NULL, 'EN COURS'),
-(239, 27, NULL, 'EN COURS'),
-(240, 17, NULL, 'EN COURS'),
-(241, 31, NULL, 'EN COURS');
+(1, 1, '23-03-2017', 'VALIDE'),
+(2, 2, '23-03-2017', 'ANNULE'),
+(3, 2, '23-03-2017', 'EN ATTENTE DE VALIDATION'),
+(4, 2, NULL, 'EN COURS'),
+(5, 3, NULL, 'EN COURS'),
+(6, 4, NULL, 'EN COURS'),
+(7, 4, NULL, 'EN COURS'),
+(8, 4, NULL, 'EN COURS'),
+(9, 4, NULL, 'EN COURS'),
+(10, 4, NULL, 'EN COURS'),
+(11, 1, '23-03-2017', 'VALIDE'),
+(12, 1, '23-03-2017', 'REFUSE'),
+(13, 1, '23-03-2017', 'EN ATTENTE DE VALIDATION'),
+(14, 1, '23-03-2017', 'EN ATTENTE DE VALIDATION'),
+(15, 1, NULL, 'EN COURS');
 
 -- --------------------------------------------------------
 
@@ -191,41 +155,13 @@ CREATE TABLE `compte` (
 
 INSERT INTO `compte` (`NUMERO_COMPTE`, `ADRESSE_MAIL`, `MOT_DE_PASSE`, `ADMIN`, `NUMERO_TELEPHONE`, `IDENTIFIANT`, `ADRESSE`, `CODE_POSTALE`, `VILLE`) VALUES
 (0, 'admin@mail.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'admin', '', 0, ''),
-(2, 'adrien-97@hotmail.fr', '098f6bcd4621d373cade4e832627b4f6', NULL, NULL, 'ashuni', '', 0, ''),
-(3, 'didi@gmail.com', 'c19c8f9b6caad92726f88434d1493ad5', NULL, NULL, 'didi', '', 0, ''),
-(4, 'lucas@lucas.com', 'dc53fc4f621c80bdc2fa0329a6123708', NULL, NULL, 'lucas', '', 0, ''),
-(5, 'gabin@gabin.com', 'dccf81a70fc86319028bc7ecb7dfb8cb', NULL, NULL, 'gabin', '', 0, ''),
-(6, 'test@gmail.com', '098f6bcd4621d373cade4e832627b4f6', NULL, NULL, 'testpseudo', '', 0, ''),
-(7, 'adrien@hotmail.fr', 'd2104a400c7f629a197f33bb33fe80c0', NULL, NULL, 'ashunibis', '15ruedelamer', 14000, 'caen'),
-(8, 'adrien@hotmail.fr', '54e66c5b67ad13dbc100aa399aae51be', NULL, NULL, 'ashunibBIS24', '15ruedelamer', 14000, 'caen'),
-(9, 'adrien@hotmail.fr', '5541c7b5a06c39b267a5efae6628e003', NULL, NULL, 'ashunibBIS25', '15 rue de la mer', 14000, 'caen'),
-(10, 'adrizfetereen@hotmail.fr', '99a30df0f2488360cdd46b4b88e5f5f0', NULL, NULL, 'ashunibBIS26', '15 rue salvador', 14000, 'caen'),
-(11, 'test@gmail.com', 'c51fe5626772cb3db97b22601e76023a', NULL, 202020202, 'editpro', 'editeur', 50000, 'professionnel'),
-(12, 'editeur@pro.com', 'a25e0e62a4702353f399953579424997', NULL, 265983265, 'editeurpro2', 'editeur', 20202, 'prof'),
-(13, 'onnel@gmail.com', 'd450c5dbcc10db0749277efc32f15f9f', NULL, 2365412563, 'Editeur', 'porfitude', 52052, 'profe'),
-(14, 'client@gmail.com', '62608e08adc29a8d6dbc9754e659f125', NULL, 2365410254, 'jeustclient', 'client', 12345, 'clientville'),
-(15, 'jeanmichel@livre.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'TestPanier', 'Adr test', 0, 'Villetest'),
-(16, 'test@test.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'test', 'test', 0, 'test'),
-(17, 'jmp@panier.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'panier', 'add', 0, 'ville'),
-(18, 'commande@gmail.com', '52dd5812b32192d0e93eb0dee465ca88', NULL, 569457684, 'commande', '1 rue de la commande', 15000, 'Commande'),
-(19, 'client@gmail.com', '62608e08adc29a8d6dbc9754e659f125', NULL, 909090909, 'client', '1 rue du client', 14000, 'Client'),
-(20, 'clientpro@gmail.com', 'af92d15dc1abab16cc30ff1bea92c6fb', NULL, 559694868, 'clientpro', '1 rue du clientpro', 14500, 'ClientPro'),
-(21, 'reiaze@gmail.com', 'aa3731263eab09d0768daaaedc22b680', NULL, 6574859034, 'testredi', '1 testredi', 14000, 'Caen'),
-(22, 'glouton@leroi.com', 'f06549a927164a3f2e336977a41794c8', NULL, 606060504, 'glouton36', 'lemanoir', 50430, 'angoville'),
-(23, 'glout@gmail.com', 'f06549a927164a3f2e336977a41794c8', NULL, 321456789, 'glouton2', 'glout', 12345, 'gloutville'),
-(24, 'glout52@gmail.com', 'f06549a927164a3f2e336977a41794c8', NULL, 321456787, 'glouton3', 'glout', 50430, 'gloutville'),
-(25, 'glouty@gmail.com', '7d2e67351b1fac82968f09e85d672374', NULL, 865327412, 'glouton4', 'glouty', 25361, 'gloutville'),
-(26, 'glout@gmail.com', '0cff9905497743137ed5c2aeb6189cac', NULL, 321456789, 'testd', 'testd', 12356, 'gloutville'),
-(27, 'leroiglouton@gmail.com', 'c275fb3307a3f36d9fbd249035e31c7c', NULL, 653241526, 'leroiglouton', 'monamis', 50430, 'gloutville'),
-(28, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'Testsousadmin', '', 0, ''),
-(29, NULL, '662be8a28444fe9b80595f92b8493fc4', NULL, NULL, 'ger1234', '', 0, ''),
-(30, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'Test12348', '', 0, ''),
-(31, 'jcpeusdo@mel.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'peusdo', 'addr', 0, 'ville'),
-(32, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'gerger', NULL, NULL, NULL),
-(33, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'GERANT1', NULL, NULL, NULL),
-(34, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'jeran', NULL, NULL, NULL),
-(35, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'jeran2', NULL, NULL, NULL),
-(36, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'jeran3', NULL, NULL, NULL);
+(1, 'Client.client@client.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'client', '678 rue des essarts', 27520, 'Bourgtheroulde'),
+(2, 'client.pro@clientpro.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'clientPro', '6 rue Anton tchekov', 14123, 'Ifs'),
+(3, 'Fournisseur@fourni.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'Fournisseur', '3 rue Anton Tchekov', 14123, 'Ifs'),
+(4, 'Fournisseur@fourni.fr', '81dc9bdb52d04dc20036dbd8313ed055', NULL, 606060606, 'Fournisseur1', '8 rue Anton Tchekhov', 14123, 'Ifs'),
+(5, NULL, '4a742ecc002db059e1500ebd73d8dce1', NULL, NULL, 'gerant_1', NULL, NULL, NULL),
+(6, NULL, '05a0bd68d7e33148fa2d51fc8b2bb99e', NULL, NULL, 'gerant_2', NULL, NULL, NULL),
+(7, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'Gerant', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -244,10 +180,7 @@ CREATE TABLE `compte_client_pro` (
 --
 
 INSERT INTO `compte_client_pro` (`NUMERO_COMPTE`, `NUMERO_PRO`, `valide`) VALUES
-(5, 123456789, 0),
-(11, 12345678912346, 0),
-(13, 12345678985241, 1),
-(20, 12345678901234, 1);
+(2, 12345678910123, 0);
 
 -- --------------------------------------------------------
 
@@ -267,10 +200,9 @@ CREATE TABLE `compte_fournisseur` (
 --
 
 INSERT INTO `compte_fournisseur` (`NUMERO_COMPTE`, `ID_EDITEUR`, `NOM_EDITEUR`, `VALIDE`) VALUES
-(27, 654123, 'glout', 1),
-(25, 666666, 'Glouton', 0),
-(23, 666667, 'Glouton', 0),
-(NULL, 917437, 'Éditions du Chameau', 1);
+(NULL, 578963, 'Edition du Chameau', 0),
+(NULL, 896541, 'Atlas', 0),
+(NULL, 897486, 'Nathan', 0);
 
 -- --------------------------------------------------------
 
@@ -287,15 +219,9 @@ CREATE TABLE `compte_gerant` (
 --
 
 INSERT INTO `compte_gerant` (`NUMERO_COMPTE`) VALUES
-(28),
-(29),
-(30),
-(31),
-(32),
-(33),
-(34),
-(35),
-(36);
+(5),
+(6),
+(7);
 
 -- --------------------------------------------------------
 
@@ -373,42 +299,14 @@ CREATE TABLE `lig_cde` (
 --
 
 INSERT INTO `lig_cde` (`NUMERO_COMMANDE`, `ISBN_ISSN`, `QTE_CMDEE`, `PRIX_UNIT`) VALUES
-(2, 9782917437520, 1, '10.00'),
-(3, 9782917437520, 5, '10.00'),
-(3, 9782917437605, 3, '15.00'),
-(4, 9782917437520, 1, '10.00'),
-(5, 9782917437520, 1, '10.00'),
-(6, 9782917437520, 1, '10.00'),
-(7, 9782917437520, 1, '10.00'),
-(12, 9782917437520, 1, '10.00'),
-(14, 9782917437520, 2, '10.00'),
-(16, 9782917437605, 1, '15.00'),
-(17, 9782917437520, 1, '10.00'),
-(19, 9782917437520, 1, '10.00'),
-(21, 9782917437605, 1, '15.00'),
-(22, 9782917437520, 1, '10.00'),
-(200, 9782917437773, 1, '15.00'),
-(201, 9782917437520, 1, '10.00'),
-(202, 9782917437742, 1, '14.00'),
-(203, 9782917437520, 1, '10.00'),
-(204, 9782917437520, 1, '10.00'),
-(205, 9782917437773, 1, '15.00'),
-(206, 9782917437520, 1, '10.00'),
-(207, 9782917437520, 1, '10.00'),
-(208, 9782917437520, 1, '10.00'),
-(209, 9782917437520, 1, '10.00'),
-(210, 9782917437520, 1, '10.00'),
-(211, 9782917437520, 1, '10.00'),
-(212, 9782917437520, 1, '10.00'),
-(213, 9782917437520, 1, '10.00'),
-(214, 9782917437520, 1, '10.00'),
-(215, 9782917437520, 1, '10.00'),
-(216, 9782917437742, 1, '14.00'),
-(216, 9782917437773, 4, '15.00'),
-(217, 9782917437520, 1, '10.00'),
-(218, 9782917437520, 1, '10.00'),
-(219, 9782917437520, 1, '10.00'),
-(219, 9782917437605, 1, '15.00');
+(1, 9782917437520, 1, '10.00'),
+(1, 9782917437605, 1, '15.00'),
+(3, 9782917437520, 1, '10.00'),
+(3, 9782917437742, 1, '14.00'),
+(11, 9782917437520, 2, '10.00'),
+(12, 9782917437742, 35, '14.00'),
+(13, 9782917437605, 1, '15.00'),
+(14, 9782917437520, 8, '10.00');
 
 -- --------------------------------------------------------
 
@@ -429,8 +327,7 @@ CREATE TABLE `note` (
 --
 
 INSERT INTO `note` (`NUMERO_COMPTE`, `TEXT`, `DATE_NOTE`, `SUPPR`, `NUM_NOTE`) VALUES
-(0, 'Test', '22/3/2017', 0, 1),
-(0, 'TEst ', '22/3/2017', 0, 2);
+(7, 'Bonjour le Campus 3', '23/3/2017', 0, 1);
 
 -- --------------------------------------------------------
 
